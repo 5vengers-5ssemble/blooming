32b0df1c257fb27a6d08a61408c0e541c31f448d
BaeYoungSuk <bysuk01@gmail.com>
2023-09-22 00:54:57 +0900

    [S09P22A105-373] refactor: FakePaymentService 수정

--- server/Blooming/src/test/java/com/fivengers/blooming/payment/application/FakePaymentService.java
+++ server/Blooming/src/test/java/com/fivengers/blooming/payment/application/FakePaymentService.java
@@ -1,10 +1,15 @@
 package com.fivengers.blooming.payment.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.fivengers.blooming.fixture.payment.adapter.out.persistence.FakePaymentPersistenceJpaAdapter;
+import com.fivengers.blooming.payment.application.port.in.dto.PaymentCompareToTempRequest;
 import com.fivengers.blooming.payment.application.port.in.dto.TempPaymentCreateRequest;
+import com.fivengers.blooming.payment.application.port.out.PaymentPort;
 import com.fivengers.blooming.payment.application.service.PaymentService;
 import com.fivengers.blooming.payment.domain.Payment;
 import com.fivengers.blooming.payment.domain.ProjectType;
+import com.mysema.commons.lang.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +17,7 @@ import org.junit.jupiter.api.Test;
 
 class FakePaymentService {
 
+    PaymentPort paymentPort;
     FakePaymentPersistenceJpaAdapter paymentPersistenceJpaAdapter;
     PaymentService paymentService;
 
@@ -31,7 +37,27 @@ class FakePaymentService {
                 .amount(100L)
                 .build();
         Payment savedPayment = paymentService.save(request);
-        Assertions.assertEquals(savedPayment.getOrderId(), request.orderId());
+        assertThat(savedPayment.getOrderId()).isEqualTo(request.orderId());
     }
+
+//    @Test
+//    @DisplayName("임시 거래 정보와 비교합니다.")
+//    void compareTempPayment() {
+//        Payment payment = Payment.builder()
+//            .id(1L)
+//            .paymentKey(null)
+//            .projectId(1L)
+//            .projectType(ProjectType.CONCERT)
+//            .done(false)
+//            .amount(500L)
+//            .orderId("newOrder")
+//            .build();
+//
+//        paymentPersistenceJpaAdapter.save(payment);
+//        PaymentCompareToTempRequest request =
+//            new PaymentCompareToTempRequest("newOrder", 500L);
+//        Boolean sameAsTemp = paymentService.compareToTempPayment(request);
+//        assertThat(sameAsTemp).isEqualTo(true);
+//    }
 
 }
