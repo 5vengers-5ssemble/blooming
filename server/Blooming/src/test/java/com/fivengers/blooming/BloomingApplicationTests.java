a0547c8f1e7e85049111254b07db6fa78a2e7d36
배영석 <bysuk01@naver.com>
2023-09-25 14:14:26 +0900

    [S09P22A105-test] chore: 일부 테스트 코드 삭제

--- server/Blooming/src/test/java/com/fivengers/blooming/BloomingApplicationTests.java
+++ server/Blooming/src/test/java/com/fivengers/blooming/BloomingApplicationTests.java
@@ -1,13 +1,13 @@
-package com.fivengers.blooming;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BloomingApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.fivengers.blooming;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BloomingApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
