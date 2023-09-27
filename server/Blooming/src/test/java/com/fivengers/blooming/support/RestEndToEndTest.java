2daff3f72f11186431230bd92af5780cb18df4b5
배영석 <bysuk01@naver.com>
2023-09-27 14:07:17 +0900

    [S09P22A105-323] chore: webEnvironment 수정

--- server/Blooming/src/test/java/com/fivengers/blooming/support/RestEndToEndTest.java
+++ server/Blooming/src/test/java/com/fivengers/blooming/support/RestEndToEndTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class RestEndToEndTest {
 
     @Autowired
